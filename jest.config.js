45664ee6bcdcbccbd1a800e7793091e2c71819b1
MatthiasMargot <matthiasmargot@hotmail.com>
2019-03-07 23:25:38 +0200

    Set up testing with Jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  transform:         {
+    '^.+\\.js?$': 'babel-jest',
+  },
+  coverageDirectory: './coverage/',
+  collectCoverage:   true,
+  coverageReporters: [
+    'json',
+    'html',
+    'json-summary',
+    'text',
+    'lcov',
+  ],
+}
