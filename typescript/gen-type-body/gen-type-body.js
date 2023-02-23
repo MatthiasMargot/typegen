dad8eb10ea5b387bc18cb851a98259fab9e46977
Edvin CANDON <edvincandon@gmail.com>
2023-02-23 09:37:48 +0100

    Support allOf, nullable & postBody types

--- typescript/gen-type-body/gen-type-body.js
+++ typescript/gen-type-body/gen-type-body.js
@@ -1,54 +1,62 @@
-const { join, pipe, map } = require('ramda')
+const { join, map, flip } = require("ramda");
 
-const genTypeProperty = require('../gen-type-property/gen-type-property')
-const genUnion = require('../gen-union/gen-union')
-const arrayHas = require('../../utils/array-has')
-const arrayLast = require('../../utils/array-last')
-const entries = require('../../utils/entries')
-const typeAliases = require('../../type-aliases')
+const genTypeProperty = require("../gen-type-property/gen-type-property");
+const genUnion = require("../gen-union/gen-union");
+const arrayHas = require("../../utils/array-has");
+const arrayLast = require("../../utils/array-last");
+const entries = require("../../utils/entries");
+const typeAliases = require("../../type-aliases");
 
-function genTypeValue (property, schemas) {
-  const { $ref, items, type } = property
+function genTypeValue(property, schemas) {
+  const { $ref, items, type } = property;
 
   if ($ref) {
-    const refName = arrayLast($ref.split('/'))
-
-    return schemas[refName].title || refName
+    const refName = arrayLast($ref.split("/"));
+    return refName;
   }
 
-  if (property.enum) {
-    return genUnion(property.enum)
-  }
+  if (property.enum) return genUnion(property.enum);
+  if (type === "array") return `${genTypeValue(items, schemas)}[]`;
 
-  if (type === 'array') {
-    return `${genTypeValue(items, schemas)}[]`
-  }
-
-  if (type === 'object') {
+  if (type === "object") {
     if (property.allOf) {
-      return join('&')(
-        map(genTypeValue)(
-          property.allOf
-        )
-      )
+      return join(" & ")(map(flip(genTypeValue)(schemas))(property.allOf));
     }
 
-    return `{ ${genTypeBody(property)} }`
+    return `{ ${genTypeBody(property, schemas)} }`;
   }
 
-  return typeAliases.ts[type] ? typeAliases.ts[type] : type
+  return typeAliases.ts[type] ? typeAliases.ts[type] : type;
 }
 
-const genTypeBody = pipe(
-  ({ properties = {}, required }, schemas) => entries(properties).map(
-    ([ key, property ]) => genTypeProperty(
-      key,
-      required && arrayHas(required, key),
-      genTypeValue(property, schemas),
-      property.description
-    )
-  ),
-  join('')
-)
+const genTypeBody = (data, schemas, unwrap = true, isPostBodyType = false) => {
+  const { properties = {}, allOf, required } = data;
 
-module.exports = genTypeBody
+  if (allOf) {
+    return join(" & ")(
+      map((entry) =>
+        entry["$ref"]
+          ? genTypeValue(entry, schemas)
+          : `{ ${genTypeBody(entry, schemas, true)} }`
+      )(allOf)
+    );
+  }
+
+  if (isPostBodyType) {
+    return genTypeValue(data, schemas);
+  }
+
+  return `${unwrap ? "" : "{"}${join("")(
+    entries(properties).map(([key, property]) =>
+      genTypeProperty(
+        key,
+        required && arrayHas(required, key),
+        property.nullable ?? false,
+        genTypeValue(property, schemas),
+        property.description ?? ""
+      )
+    )
+  )}${unwrap ? "" : "}"}`;
+};
+
+module.exports = genTypeBody;
